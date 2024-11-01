4711fa6b10ec4043d8a2cd8633a50589617f71d5
“pavelFaraday” <devstudio.corp@gmail.com>
2024-11-01 13:15:34 +0400

    37. Creating Relationships Between Content

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -1,6 +1,7 @@
 <?php 
   // Create Event CPT
 function university_post_types() {
+    // Event Post Type
     register_post_type('events',
         array (
             'public' => true,
@@ -18,6 +19,26 @@ function university_post_types() {
             )
         )
     );
+
+    // Program Post Type
+    register_post_type('program',
+    array (
+        'public' => true,
+        'show_in_rest' => true,
+
+        'has_archive' => true,
+        'rewrite' => array('slug' => 'programs'),
+        'supports' => array('title', 'editor'),
+        'labels' => array (
+            'name' => __('Programs'),
+            'add_new_item' => 'Add New Program',
+            'edit_item' => 'Edit Program',
+            'all_items' => 'All Programs',
+            'singular_name' => __('Program')
+        ),
+        'menu-icon' => 'dashicons-awards',
+    )
+);
   }
   add_action('init', 'university_post_types');
 ?>
