c8d6f218bddcffdc3ae713d44e4fc391e5a43d96
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2024-11-14 21:53:19 +0400

    63. Keyboard Events in JavaScript

--- app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
+++ app/public/wp-content/themes/fictional-university-theme/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery'), 'version' => '7ab4bea5b1ec7f107263');
+<?php return array('dependencies' => array('jquery'), 'version' => '29dc6acc5974511e1e6a');
--- app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -7,6 +7,8 @@ class Search {
         this.closeButton =  $(".search-overlay__close");
         this.searchOverlay =  $(".search-overlay");
 
+        this.isOverlayOpen = false;
+
         this.events(); // call "events" method
     }
 
@@ -14,14 +16,29 @@ class Search {
     events(){
         this.openButton.on("click", this.openOverlay.bind(this));
         this.closeButton.on("click", this.closeOverlay.bind(this));
+        $(document).on("keydown", this.keyPressDispatcher.bind(this));
     }
 
     // 3. Methods (functions, action..)
+    keyPressDispatcher(e){
+       if (e.keyCode == 83 && !this.isOverlayOpen) {
+        this.openOverlay();
+       }
+       if (e.keyCode == 27 && this.isOverlayOpen) {
+        this.closeOverlay();
+       }
+    }
     openOverlay() {
         this.searchOverlay.addClass("search-overlay--active");
+        $("body").addClass("body-no-scroll");
+        console.log("open method just ran!");
+        this.isOverlayOpen = true;
     }
     closeOverlay() {
         this.searchOverlay.removeClass("search-overlay--active");
+        $("body").removeClass("body-no-scroll");
+        console.log("close method just ran!");
+        this.isOverlayOpen = false;
     }
 }
 
