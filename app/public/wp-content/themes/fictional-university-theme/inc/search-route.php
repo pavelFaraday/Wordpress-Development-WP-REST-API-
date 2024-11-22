620e3f964d077697aa6b506ae36239fb6f1147e2
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2024-11-22 13:14:43 +0400

    75. Create Your Own Raw JSON Data

--- app/public/wp-content/themes/fictional-university-theme/inc/search-route.php
+++ app/public/wp-content/themes/fictional-university-theme/inc/search-route.php
@@ -14,6 +14,17 @@ function universitySearchResults() {
     'post_type' => 'professor'
   ));
 
-  return $professors->posts;
+  $professorResults = array();
+
+  while($professors->have_posts()){
+    $professors->the_post();
+    array_push($professorResults, array(
+      'title' => get_the_title(),
+      'permalink' => get_the_permalink(),
+      
+    ));
+  }
+
+  return $professorResults;
 }
 ?>
