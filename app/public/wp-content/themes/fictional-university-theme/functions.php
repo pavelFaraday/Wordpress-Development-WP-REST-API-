f7dccf0a0b752b63627ebaf40854bcdba7b46e16
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2024-12-04 15:26:55 +0400

    88. Open Registration

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -63,6 +63,24 @@ function university_adjust_queries ($query) {
 }
 add_action('pre_get_posts', 'university_adjust_queries');
 
+// Redirect subscriber accounts out of admin and onto homepage
+function redirectSubsToFrontend() {
+  $ourCurrentUser = wp_get_current_user();
+  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+    wp_redirect(site_url('/'));
+    exit;
+  }
+}
+add_action('admin_init', 'redirectSubsToFrontend');
+
+function noSubsAdminBar() {
+  $ourCurrentUser = wp_get_current_user();
+  if (count($ourCurrentUser->roles) == 1 AND $ourCurrentUser->roles[0] == 'subscriber') {
+    show_admin_bar(false);
+  }
+}
+add_action('wp_loaded', 'noSubsAdminBar');
+
 // Reusable Code
 function pageBanner ($args = NULL) { 
   if (!isset($args['title'])) {
@@ -89,5 +107,7 @@ function pageBanner ($args = NULL) {
   </div>  
 </div>
 <?php } ?>
+
+
 
 
