bf676c1931ad4c50f98a691f3738ed3cc3a718a0
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2024-12-04 13:48:02 +0400

    Create Coonditional Logic for LOGIN/LOGOUT functionality

--- app/public/wp-content/themes/fictional-university-theme/header.php
+++ app/public/wp-content/themes/fictional-university-theme/header.php
@@ -22,8 +22,14 @@
           </ul>
         </nav>
         <div class="site-header__util">
-          <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
-          <a href="<?php echo esc_url(site_url('/wp-signup.php')); ?>" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>
+          <?php 
+         if (is_user_logged_in()) { ?>
+           <a href="<?php echo wp_logout_url(); ?>" class="btn btn--small  btn--dark-orange float-left">Log Out</a>
+  <?php  } else { ?>
+           <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+           <a href="<?php echo esc_url(site_url('/wp-signup.php')); ?>" class="btn btn--small  btn--dark-orange float-left">Sign Up</a>
+   <?php } ?>
+          
           <a href="<?php echo esc_url(site_url('/search')); ?>" class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></a>
         </div>
       </div>
