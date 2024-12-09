e123ed200f5b4c4fdc83d303bd7ad5718f0da897
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2024-12-09 12:34:57 +0400

    Limit User Notes Quantity with "count_user_posts()" func

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -110,6 +110,9 @@ add_filter('login_headertitle', 'ourLoginTitle');
 /* -------------------------------------------------------------------------- */
 function makeNotePrivate($data) {
   if ($data['post_type'] == 'note') {
+    if (count_user_posts(get_current_user_id(),"note") >= 5) {
+      die("You have reached your note limit.");
+    }
     $data['post_content'] = sanitize_textarea_field($data['post_content']);
     $data['post_title'] = sanitize_text_field($data['post_title']);
   }
