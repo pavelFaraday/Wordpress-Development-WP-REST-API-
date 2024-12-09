12cd6bc118fe22156df9ccd247c65338d39f7943
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2024-12-09 11:32:28 +0400

    99. Note Permissions and Security

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -109,6 +109,10 @@ add_filter('login_headertitle', 'ourLoginTitle');
 /*                       Force note posts to be private                       */
 /* -------------------------------------------------------------------------- */
 function makeNotePrivate($data) {
+  if ($data['post_type'] == 'note') {
+    $data['post_content'] = sanitize_textarea_field($data['post_content']);
+    $data['post_title'] = sanitize_text_field($data['post_title']);
+  }
   if($data['post_type'] == 'note' AND $data['post_status'] !== 'trash'){
     $data['post_status'] = "private";
   }
