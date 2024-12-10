b016f4fc59e3db93b6249ea5d3bbbffee36d2e47
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2024-12-10 10:56:48 +0400

    Create Heart Like CPT

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -99,6 +99,22 @@ function university_post_types() {
   ));
 
 
-}
+    // Like Post Type
+    register_post_type('like', array(
+      'supports' => array('title'),
+      'public' => false,
+      'show_ui' => true,
+      'labels' => array(
+        'name' => 'Likes',
+        'add_new_item' => 'Add New Like',
+        'edit_item' => 'Edit Like',
+        'all_items' => 'All Likes',
+        'singular_name' => 'Like'
+      ),
+      'menu_icon' => 'dashicons-heart'
+    ));
+
+
+  }
   add_action('init', 'university_post_types');
 ?>
