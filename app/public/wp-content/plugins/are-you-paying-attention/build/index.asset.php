69464276abe168cd74f5d8b08d4c64b1d653180d
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2025-02-11 11:17:48 +0400

    141. Starting Our Multiple Choice Block Type

--- app/public/wp-content/plugins/are-you-paying-attention/build/index.asset.php
+++ app/public/wp-content/plugins/are-you-paying-attention/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime'), 'version' => 'a8a1a7e0663e0895d2cd');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components'), 'version' => '581c59ffacbc8b37c390');
