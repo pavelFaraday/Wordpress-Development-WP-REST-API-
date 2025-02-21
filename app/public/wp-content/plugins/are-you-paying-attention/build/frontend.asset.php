16f62c606b07f5ab2e18196ad47ab1a334dff035
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2025-02-21 14:58:11 +0400

    152. Let Admin Choose Background Color of Block

--- app/public/wp-content/plugins/are-you-paying-attention/build/frontend.asset.php
+++ app/public/wp-content/plugins/are-you-paying-attention/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '7200e9383f1bb0e6887e');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime'), 'version' => '8fb3b309fab161a9e6f5');
